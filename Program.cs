f3902bad0fb76e482d905f2146991611b1402824
Diogo-Coelho-Nunes <nunes.diogo01@gmail.com>
2022-05-16 19:49:03 +0100

    Adicionei duas var

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var teste1 = "Olá, isto é C#.";
+var teste2 = "Sou um Rookie.";
+
+Console.WriteLine(teste1 + " " + teste2);
