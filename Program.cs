d6c96bd788fdd2023a17f3924a52624e4661a699
Diogo Coelho Nunes <105322991+Diogo-Coelho-Nunes@users.noreply.github.com>
2022-05-16 19:50:30 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,5 +3,6 @@ Console.WriteLine("Hello, World!");
 
 var teste1 = "Olá, isto é C#.";
 var teste2 = "Sou um Rookie.";
-
+var teste3 = "Enviar para Visual Studio";
 Console.WriteLine(teste1 + " " + teste2);
+Console.WriteLine(teste3);
