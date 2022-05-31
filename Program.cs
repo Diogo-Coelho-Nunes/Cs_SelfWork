77f81b91272b9a7b7449b02eed46648a853ebcd1
Diogo Coelho Nunes <105322991+Diogo-Coelho-Nunes@users.noreply.github.com>
2022-05-31 20:19:18 +0100

    Commited from desktop

--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@ Console.WriteLine("Hello, World!");
 var teste1 = "Olá, isto é C#.";
 var teste2 = "Sou um Rookie.";
 var teste3 = "Enviar para Visual Studio.";
+var teste4 = "GitHub_Desktop"
 Console.WriteLine(teste1 + " " + teste2);
 Console.WriteLine(teste3);
